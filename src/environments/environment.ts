ed8ca5c43bb665797163df15f4dc3588721306c7
Aingeru Sanchez <Aingeru1703@gmail.com>
2019-07-03 00:29:12 +0200

    ADD Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false,
   development: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAq98E7w4LAkYq2Hh2OwBWejVPaEIpRFsE',
+    authDomain: 'photoroll.firebaseapp.com',
+    databaseURL: 'https://photoroll.firebaseio.com',
+    projectId: 'photoroll',
+    storageBucket: 'photoroll.appspot.com',
+    messagingSenderId: '670756297560',
+    appId: '1:670756297560:web:1903ef6f0808fa5c',
+  }
 };
 
 /*
