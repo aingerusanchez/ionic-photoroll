35acfea070c68015cedd756fac086f26e2f49bf7
Aingeru Sanchez <Aingeru1703@gmail.com>
2019-07-03 23:09:04 +0200

    HOME: skeleton

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
+import { FirebaseService } from './../../services/firebase.service';
 import { Observable } from 'rxjs';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { UploadPage } from '../upload/upload.page';
 import { ModalController } from '@ionic/angular';
@@ -10,18 +11,25 @@ import { AngularFireDatabase } from '@angular/fire/database';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  public posts: Observable<any>;
+  public posts: Observable<any> = null;
   protected readonly debug = environment.development;
   private readonly CLASS_NAME = 'HomePage';
 
   constructor(
     private modalCtrl: ModalController,
     private db: AngularFireDatabase,
+    // private firebase: FirebaseService, // TODO: Usar FirebaseService
   ) {
     console.debug(`${this.CLASS_NAME}.constructor()`);
-    this.posts = db.list('post').valueChanges();
+    setTimeout( () => {
+      this.posts = db.list('post').valueChanges();
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+    // this.firebase.getPosts();  // TODO: Usar FirebaseService
   }
 
   /** Abre la página para poder subir una nueva publicación */
