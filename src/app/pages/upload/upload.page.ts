9491b5be600e7036800c0c0817b6a0440fe2750f
Aingeru Sanchez <Aingeru1703@gmail.com>
2019-07-28 14:31:20 +0200

    NEW PAGE(UploadPage)

--- src/app/pages/upload/upload.page.ts
+++ src/app/pages/upload/upload.page.ts
@@ -9,24 +9,26 @@ import { environment } from 'src/environments/environment';
 })
 export class UploadPage implements OnInit {
 
-  public debug = environment.development;
-  public titulo: string;
-
+  public titulo = '';
+  protected debug = environment.development;
   private CLASS_NAME = 'UploadPage';
 
   constructor(private modalCtrl: ModalController) {
     console.debug(`${this.CLASS_NAME}.constructor()`);
-    this.titulo = '';
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   /** Cierra la pantalla modal en primer plano */
   public dismissModal = () => {
     console.debug(`${this.CLASS_NAME}.dismissUploadModal()`);
 
     this.modalCtrl.dismiss();
+  }
+
+  submit() {
+    console.log('UploadPage -> submit', this.titulo);
+
   }
 
 }
